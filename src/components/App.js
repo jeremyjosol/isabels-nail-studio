367f83fa98c8cb186dc590591346777fe81f4d8c
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-04 15:26:04 -0800

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import SocialMedia from './components/SocialMedia';
-import ContactForm from './components/ContactForm';
+import Header from './Header';
+import About from './/About';
+import SocialMedia from './SocialMedia';
+import ContactForm from './ContactForm';
 
 const App = () => {
   return (
     <React.Fragment>
-    <Header></Header>
-    <About></About>
-    <SocialMedia></SocialMedia>
-    <ContactForm></ContactForm>
+    <Header />
+    {/* <About />
+    <SocialMedia />
+    <ContactForm /> */}
     </React.Fragment>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+    <header>
+      <nav>
+        Home
+        About
+        Social Media
+        Contact
+      </nav>
+    </header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
