2382f29aabeb83a37cd29f56861f0e1b5d9014d0
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-05 12:25:34 -0800

    Work on features

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,18 +5,18 @@ import { InstagramEmbed } from 'react-social-media-embed';
 const SocialMedia = () => {
   return (
     <section className="social-media">
-      <br />
-        <a
-          href="https://www.instagram.com/nails.tubbytoast"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className='instagram'><FaInstagram className='icons' /> Instagram </span> |
-          <span className='handle'> @nails.tubbytoast</span>
-        </a>
-        <div className='feed'>
-          <InstagramEmbed url="https://www.instagram.com/nails.tubbytoast" width={328} />
-        </div>
+      <a
+        href="https://www.instagram.com/nails.tubbytoast"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h3 className='instagram'>
+          <FaInstagram className='icons' /> Instagram
+        </h3>
+      </a>
+      <div className='feed'>
+        <InstagramEmbed url="https://www.instagram.com/nails.tubbytoast" width={328} />
+      </div>
     </section>
   );
 };
