a11c13c0931138434b03ed5184f459c6a21173b5
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-04 18:38:18 -0800

    Update Services data

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,18 +4,26 @@ const Services = () => {
   const servicesData = [
     {
       id: 1,
-      name: 'Korean/Japanese Gel Extensions',
-      description: '',
+      name: 'Tubby Gel Mani',
+      description: 'Dry manicure on natural nails. Includes detailed cuticle work with an efile. Includes 1 color of Korean or Japanese gel polish.',
+      price: 60
     },
     {
       id: 2,
-      name: 'Soft Gel Overlays',
-      description: '',
+      name: 'Tubby Overlay',
+      description: 'Add strength and durability to your natural nails. This is an add on to the Tubby Gel Mani',
+      price: 30
     },
     {
       id: 3,
-      name: 'Gel Nail Art',
-      description: '',
+      name: 'Gel Extensions',
+      description: 'Offered in short-XXL length. This is an alternative to acrylic nails. Includes 1 color of Korean or Japanese gel polish (+10 for XL or longer).',
+      price: 100
+    },
+    {
+      id: 4,
+      name: 'Custom Nail Art',
+      description: 'Prices vary depending on design and how detailed/elaborate it is. Contact to receive a quote. Pictures for inspiration are encouraged.',
     },
   ];
 
@@ -26,7 +34,7 @@ const Services = () => {
           <div 
             key={service.id} 
             className="service-item">
-            <h3>{service.name}</h3>
+            <h3>{service.name} | ${service.price}</h3>
             <p className='light'>{service.description}</p>
           </div>
         ))}
