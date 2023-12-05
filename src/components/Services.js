6b8b6a1acd9d9adfc2efa508e30c282f719798d5
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-04 23:21:18 -0800

    Implement photo card w/ text hovered over & centered

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import nailPhoto from './../img/nail-art.jpg';
 
 const Services = () => {
   const servicesData = [
@@ -6,7 +7,8 @@ const Services = () => {
       id: 1,
       name: 'Tubby Gel Mani',
       description: 'Dry manicure on natural nails. Includes detailed cuticle work with an efile. Includes 1 color of Korean or Japanese gel polish.',
-      price: 60
+      price: 60,
+      image: nailPhoto
     },
     {
       id: 2,
@@ -27,18 +29,60 @@ const Services = () => {
     },
   ];
 
+  const [selectedService, setSelectedService] = useState(null);
+
+  const handleServiceClick = (service) => {
+    setSelectedService(service);
+  };
+
+  const handleCloseDetails = () => {
+    setSelectedService(null);
+  };
+
   return (
     <section className="services">
       <div className="services-list">
         {servicesData.map((service) => (
-          <div 
-            key={service.id} 
-            className="service-item">
-            <h3>{service.name} | ${service.price}</h3>
-            <p className='light'>{service.description}</p>
+          <div
+            key={service.id}
+            className="service-card"
+            onClick={() => handleServiceClick(service)}
+          >
+            <div
+              className="service-image"
+              style={{
+                backgroundImage: `url(${service.image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                width: '350px',
+                height: '350px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
+              }}
+            >
+              <div className="service-name">
+                <span style={{ 
+                  position: 'absolute', 
+                  left: '50%', 
+                  top: '50%', 
+                  transform: 'translate(-50%, -50%)' }}>
+                  {service.name}
+                </span>
+              </div>
+            </div>
           </div>
         ))}
       </div>
+
+      {selectedService && (
+        <div className="service-details">
+          <h3>{selectedService.name} | ${selectedService.price}</h3>
+          <p className="light">{selectedService.description}</p>
+          <button onClick={handleCloseDetails}>Close</button>
+        </div>
+      )}
     </section>
   );
 };
