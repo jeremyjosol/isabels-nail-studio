132329491ccbd94f95f48c1a9b7db50fc3ff4d82
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-04 16:48:03 -0800

    Add basic styles + logo & header in Korean

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '.././img/logo.jpg';
 
 const Header = () => {
   return (
     <header>
-      <div className="tubby-toast">Tubby Toast</div>
+      <div className="tubby-toast">
+       <h1>통통한 토스트</h1>
+        <img src={logo} alt='logo' className='logo-image' />
+      </div>
       <nav>
         <Link to="/about">About</Link>
         <Link to="/social-media">Social Media</Link>
