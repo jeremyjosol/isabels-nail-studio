90f2d4bd3a2d237574ff4bccd3a74f7dc222bdc9
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-04 16:25:40 -0800

    Add import & initialize form state + form handling

---
+++ src/components/ContactForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Test data:', formData);
+  };
+}
+
+export default ContactForm;
