2af2784443394b3df5da0b0de78f9a9fe9acdfc9
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-04 16:10:50 -0800

    Add import + initialize state & add boilerplate info

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 const About = () => {
+  const [isDetailsVisible, setDetailsVisibility] = useState(false);
+
+  const toggleDetails = () => {
+    setDetailsVisibility(!isDetailsVisible);
+  };
+
   return (
-   <div className='about'>
-    Test
-   </div>
+    <section className="about">
+      <h2>About Tubby Toast</h2>
+      <p>
+        Custom Korean/Japanese manicures based in Portland, Oregon.
+      </p>
+    </section>
   );
 };
 
