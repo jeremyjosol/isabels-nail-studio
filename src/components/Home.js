4cace512a162ad8dfa8ab4998332e23ca8a14fa5
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-06 17:17:27 -0800

    Work on Hero features

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+
 const Home = () => {
-  return null;
+  return (
+    <div className="hero">
+      <h1><strong>PORTLAND</strong> BASED STUDIO</h1>
+      <h2>KOREAN NAIL ARTIST</h2>
+      <h3><strong>SPECIALIZING</strong> IN <span className='hero-color'>HIGH-QUALITY</span></h3>
+      <h3><span class='hero-color'>CREATIVE</span> GEL <strong>MANICURES</strong></h3>
+      <br />
+      <Link to="/about" className="btn btn-dark explore">
+        LEARN MORE <MdKeyboardDoubleArrowRight />
+      </Link>    
+    </div>
+  );
 };
 
 export default Home;
