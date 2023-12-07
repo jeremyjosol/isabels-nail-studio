ba31a1f3ad3471ef5771e474f3bac9844d341ce9
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-06 22:24:50 -0800

    Install react-bootstrap + implement Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '.././img/logo.jpg';
-import { FaStar } from "react-icons/fa6";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../img/logo.jpg';
+import { FaStar } from 'react-icons/fa6';
 
 const Header = () => {
   return (
     <header>
-      <div className='tubby-toast'>
-        <div className="logo-container">
-          <Link to='/' >
-            <h2 className='brand-symbols'>통통한 토스트</h2>
-          </Link>
-          <Link to='/'>
-            <img src={logo} alt='logo' className='logo-image' />
-          </Link>
-          <Link to='/' >
-            <h1 className='brand'><FaStar className='icons' /> Tubby Toast <FaStar className='icons' /></h1>
-          </Link>
-        </div>
-      </div>
-      <nav>
-        <Link to='/about'>About</Link>
-        <Link to='/services'>Services</Link>
-        <Link to='/social-media'>Social Media</Link>
-        <Link to='/contact'>Contact</Link>
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <Link to='/'>
+              <h2 className='brand-symbols'>통통한 토스트</h2>
+            </Link>
+            <Link to='/'>
+              <img src={logo} alt='logo' className='logo-image' />
+            </Link>
+            <Link to='/'>
+              <h1 className='brand'><FaStar className='icons' /> Tubby Toast <FaStar className='icons' /></h1>
+            </Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to='/about'>About</Nav.Link>
+              <Nav.Link as={Link} to='/services'>Services</Nav.Link>
+              <Nav.Link as={Link} to='/social-media'>Social Media</Nav.Link>
+              <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
       <hr className='line' />
     </header>
   );
 };
 
 export default Header;
+
